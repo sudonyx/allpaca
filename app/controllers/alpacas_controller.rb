8dde81c818e8a92169a294611d230dfe47e53f7e
Benjamin Malin <benjamin.malin047@gmail.com>
2023-08-27 14:35:46 +0100

    updated alpaca new form, slight change to controller also

--- app/controllers/alpacas_controller.rb
+++ app/controllers/alpacas_controller.rb
@@ -17,6 +17,16 @@ class AlpacasController < ApplicationController
   private
 
   def alpaca_params
-    params.require(:alpaca).permit(:name, :colour, :hat, :location, :price_per_night, :user_id, :photo)
+    params.require(:alpaca).permit(:name, :colour, :hat, :location, :price_per_night, :user_id, :photo, :image)
+  end
+
+  if @alpaca.save
+    if params[:alpaca][:image].present?
+      result = Cloudinary::Uploader.upload(params[:alpaca][:image])
+      @alpaca.update(image_url: result['secure_url'])
+    end
+    redirect_to @alpaca, notice: 'Alpaca was successfully created.'
+  else
+    render :new
   end
 end
