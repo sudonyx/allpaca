646dcf2177280aa4293524078f1ba5f1269b5e00
Bay Si <xuebeisi@gmail.com>
2023-08-29 19:15:01 +0200

    landing page is done

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'pages#home'
+  get '/apply', to: 'alpacas#new', as: 'apply'
 
   resources :alpacas, only: %i[index show new create destroy]
-
 end
