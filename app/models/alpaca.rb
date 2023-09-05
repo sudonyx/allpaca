b421204a75b9640391b5f8e371d329fb19930869
Josh Wills <jcwills369@gmail.com>
2023-09-05 19:26:18 +0100

    add validations to models

--- app/models/alpaca.rb
+++ app/models/alpaca.rb
@@ -4,4 +4,11 @@ class Alpaca < ApplicationRecord
   has_many :reviews, through: :bookings, dependent: :destroy
 
   has_one_attached :photo
+  validates :photo, presence: true
+
+  validates :name, presence: true
+  validates :colour, presence: true
+  validates :location, presence: true
+  validates :hat, presence: true
+  validates :price_per_night, numericality: { greater_than: 0 }
 end
