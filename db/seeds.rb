029b1e93e1a23677d50800e43277c76c0cd32488
Josh Wills <jcwills369@gmail.com>
2023-09-06 14:59:53 +0100

    update seeds to add more users

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,13 @@
 Alpaca.destroy_all
 User.destroy_all
 
-user = User.create!({ email: 'test@test.com', password: 'password' })
+user1 = User.create!({ username: "test" email: 'test@test.com', password: 'password' })
+user2 = User.create!({ username: "Josh" email: 'josh@test.com', password: 'password' })
+user3 = User.create!({ username: "Bay" email: 'bay@test.com', password: 'password' })
+user4 = User.create!({ username: "Ben" email: 'ben@test.com', password: 'password' })
+user5 = User.create!({ username: "James" email: 'james@test.com', password: 'password' })
 
-30.times do
+40.times do
   name = Faker::Artist.name
   colour = Faker::Color.color_name.capitalize
   hat = ['Trilby', 'Cowboy', 'Wizard Hat', 'Safety Helmet', 'Graduation Cap', 'Pied Piper Hat', 'Crown'].sample
@@ -19,6 +23,6 @@ user = User.create!({ email: 'test@test.com', password: 'password' })
   price_per_night = (20..100).to_a.sample
 
   alpaca = Alpaca.new({ name: name, colour: colour, hat: hat, location: location, price_per_night: price_per_night })
-  alpaca.user = user
+  alpaca.user = [user1, user2, user3, user4, user5].sample
   alpaca.save!
 end
